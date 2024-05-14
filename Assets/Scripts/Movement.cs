9048e8d99719f85873656fd22d6fcdc9347b7075
phuuuc <125535950+phuuuc@users.noreply.github.com>
2024-05-14 12:17:22 -0700

    added more complex movement controls

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,9 +5,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 12f;
+
+    private float runMaxSpeed = 12f;
+    private float runAccelAmount = 12f;
+    private float runDeccelAmount = 24f;
+    private float accelInAir = 0.5f;
+    private float deccelInAir = 0.5f;
+    private bool doConserveMomentum = true;
+
+    private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+
+    private float jumpingForce = 16f;
+
     private bool isFacingRight = true;
+    private bool isJumping;
+    private float lastOnGroundTime = 0.1f;
+    private float coyoteTime = 0.2f;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -15,32 +29,77 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private AudioClip jumpClip;
     [SerializeField] private AudioClip walkClip;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
+        lastOnGroundTime -= Time.deltaTime;
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            SoundFXManager.instance.PlaySoundFXClip(jumpClip, transform, 0.5f);
-        }
+        if (IsGrounded())
+            lastOnGroundTime = coyoteTime;
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-
+        Jump();    
         Flip();
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        Run();
+    }
+
+    private void Run()
+    {
+        float targetSpeed = horizontal * runMaxSpeed;
+        float accelRate;
+
+        if (lastOnGroundTime > 0)
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? runAccelAmount : runDeccelAmount;
+        else
+            accelRate = (Mathf.Abs(targetSpeed) > 0.0f) ? runAccelAmount * accelInAir : runAccelAmount * deccelInAir;
+        
+        if (doConserveMomentum && Mathf.Abs(rb.velocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(rb.velocity.x) ==
+                Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && lastOnGroundTime < 0)
+            accelRate = 0;
+        
+        float speedDiff = targetSpeed - rb.velocity.x;
+        float movement = speedDiff * accelRate;
+
+        rb.AddForce(movement * Vector2.right, ForceMode2D.Force);
     }
 
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferCounter = jumpBufferTime;
+        }
+        else
+        {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if (lastOnGroundTime > 0f && jumpBufferCounter > 0f && !isJumping)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingForce);
+            //SoundFXManager.instance.PlaySoundFXClip(jumpClip, transform, 0.5f);
+            jumpBufferCounter = 0f;
+            StartCoroutine(JumpCooldown());
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+            lastOnGroundTime = 0f;
+        }
     }
 
     private void Flip()
@@ -51,7 +110,13 @@ public class PlayerMovement : MonoBehaviour
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
-            // SoundFXManager.instance.PlaySoundFXClip(walkClip, transform, 1f);
         }
+    }
+
+    private IEnumerator JumpCooldown()
+    {
+        isJumping = true;
+        yield return new WaitForSeconds(0.4f);
+        isJumping = false;
     }
 }
