66e475802b6d21d7ea2eb454400074bb60e75beb
km <66489804+km1h@users.noreply.github.com>
2024-05-30 14:46:49 -0700

    Updated Puzzle

--- Assets/Scripts/Kill_player.cs
+++ Assets/Scripts/Kill_player.cs
@@ -7,27 +7,27 @@ public class Kill_player : MonoBehaviour
     public GameObject player;
     public Transform respawnPoint;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //void Start()
+    //{
+
+    //}
 
     // Update is called once per frame
-    void Update()
+    //void Update()
+    //{
+    //    if (player.transform.position.y <= transform.position.y)
+    //    {
+    //        player.transform.position = respawnPoint.position;
+    //    }
+    //}
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (player.transform.position.y <= transform.position.y)
-        {
-            player.transform.position = respawnPoint.position;
-        }
-    }
-    /*
-    private void OnCollisonEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             player.transform.position = respawnPoint.position;
 
         }
     }
-    */
+    
 }
